1bcbb519713cee3a63f9688456e84f946d0bd0ca
Ashkan Arabi <ashkan.arabim@gmail.com>
2024-12-20 09:07:53 -0700

    Gemini 2.0 support (#27)
    * merge server gitignore into global gitignore
    
    * reimplement llm in fastapi
    
    * add gemini api support to llm server
    
    * format llm_server.py
    
    * adjust llm_classes.py comments
    
    * add new llm server to docker & docker compose
    
    * add phrase-level yielding to llama model class
    
    * add phrase-level yielding to gemini2 class
    
    * add support for gemini2 to sserver.py
    
    * apply black formatting to server.py
    
    * add ollama installation to llm Dockerfile
    
    * bugfix: fix llm.py yielding binary strings to server
    
    * add gemini instructions to .env.prod

---
+++ llm/src/llm_server.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+import uvicorn
+from contextlib import asynccontextmanager
+import os
+import llm_classes
+
+
+model = None
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup and shutdown events
+    # see https://fastapi.tiangolo.com/advanced/events/
+
+    global model
+
+    # startup
+    # get model name
+    model_name = os.environ.get("MODEL_NAME")
+    assert model_name is not None, "MODEL_NAME env variable not set!"
+
+    if model_name.startswith("llama"):
+        model = llm_classes.Llama(llama_name=model_name)
+    elif model_name.startswith("gemini"):
+        gemini_model_name, api_key = model_name.split("|")
+
+        model = llm_classes.Gemini2(model_name=gemini_model_name, api_key=api_key)
+    else:
+        raise Exception("Invalid model name:", model_name)
+
+    # app starts here
+    yield
+
+    # shutdown
+    # empty for now...
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/api/generate")
+def generate(prompt: str):
+    def generate_stream():
+        for phrase in model.generate(prompt):
+            phrase = phrase.encode()
+            yield phrase
+
+    return StreamingResponse(generate_stream())
+
+
+if __name__ == "__main__":
+    port = os.environ.get("LLM_PORT")
+    assert port is not None, "LLM_PORT in llm-server is not set!"
+    port = int(port)
+
+    is_dev = os.environ.get("IS_DEV", "false").lower() == "true"
+
+    uvicorn.run(__name__ + ":app", host="0.0.0.0", port=port, reload=is_dev)
--- server/src/llm.py
+++ server/src/llm.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+import aiohttp
 from ollama import AsyncClient
 from langcodes import Language
 
@@ -19,22 +20,33 @@ Here's the story:
 
 
 LLM_SERVER = os.environ.get("LLM_SERVER")
-if not LLM_SERVER:
-    raise Exception("LLM_SERVER env var not set!")
+assert LLM_SERVER, "LLM_SERVER env var not set!"
 
 
 client = AsyncClient(host=LLM_SERVER)
 
 
 async def summarize_news(content: str, lang="en"):
-    prompt = "\n".join([
-        # converts language code to display name before replacing
-        INSTRUCTIONS.replace("<<LANGUAGE>>", Language.get(lang).display_name()),
-        '"""',
-        content,
-        '"""',
-    ])
-    
-    generator = await client.generate(model="llama3.2", prompt=prompt, stream=True)
-    async for chunk in generator:
-        yield chunk
+    prompt = "\n".join(
+        [
+            # converts language code to display name before replacing
+            INSTRUCTIONS.replace("<<LANGUAGE>>", Language.get(lang).display_name()),
+            '"""',
+            content,
+            '"""',
+        ]
+    )
+
+    # call llm service
+    # example call:
+    # curl localhost:11000/api/generate?prompt=tell+me+a+story+that+is+peak+fiction.
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            f"http://{LLM_SERVER}/api/generate?prompt={prompt}"
+        ) as response:
+            if response.status != 200:
+                raise Exception(f"LLM server returned {response.status} on {prompt}")
+
+            async for phrase in response.content:
+                phrase = phrase.decode() # decode utf-8 sequence to string
+                yield phrase
